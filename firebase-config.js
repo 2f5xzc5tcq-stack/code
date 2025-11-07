cfbabdc97f89f991dc0caab7fc5de144e6dd9702
2f5xzc5tcq-stack <2f5xzc5tcq@privaterelay.appleid.com>
2025-11-08 02:36:19 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,13 @@
-// Firebase Configuration - PRIVATE
-// File này chứa config thật, KHÔNG push lên GitHub!
-
 const firebaseConfig = {
   apiKey: "AIzaSyCGsy5DvM95clZLQJLajkIKtqCPLoWEiqY",
   authDomain: "quiz-be35a.firebaseapp.com",
-  databaseURL: "https://quiz-be35a-default-rtdb.firebaseio.com",
+  databaseURL: "https://quiz-be35a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "quiz-be35a",
   storageBucket: "quiz-be35a.firebasestorage.app",
   messagingSenderId: "273159571040",
   appId: "1:273159571040:web:99ba579643d010f42f6505"
 };
 
-// Initialize Firebase
 if (typeof firebase !== 'undefined') {
   firebase.initializeApp(firebaseConfig);
 }
